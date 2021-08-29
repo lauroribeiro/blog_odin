f7cb0b88761296993df2a7c80b9c384812454494
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-08-28 23:18:51 -0300

    create new action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      flash[:success] = "Your article as sucessfuly created!"
+      redirect_to @article
+    else
+      flash.now[:error] = "Your article couldn't be created!"
+      render :new
+    end
+  end
 end
